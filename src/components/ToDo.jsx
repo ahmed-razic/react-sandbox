fc88ca0e046390c5f2af6c03f8319874a494698c
ahmed-razic <ahmed.razic@gmail.com>
2022-01-20 21:19:48 +0100

    added Third example

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,5 +1,30 @@
+import { useState, useEffect, useRef } from 'react';
+
 function ToDo() {
-  return <div>ToDo</div>;
+  const [loading, setLoading] = useState(true);
+  const [todo, setTodo] = useState({});
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    console.log(isMounted);
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => response.json())
+      .then((data) => {
+        setTimeout(() => {
+          if (isMounted.current) {
+            setTodo(data);
+            setLoading(false);
+          }
+        }, 3000);
+      });
+
+    return () => {
+      isMounted.current = false;
+      console.log(isMounted);
+    };
+  }, [isMounted]);
+
+  return loading ? <h3>Loading...</h3> : <h1>{todo.title}</h1>;
 }
 
 export default ToDo;
--- src/components/UseRefExample3.jsx
+++ src/components/UseRefExample3.jsx
@@ -2,8 +2,21 @@ import { useState } from 'react';
 import ToDo from './ToDo';
 
 function UseRefExample3() {
-  const [showToDo, setShowToDo] = useState(true);
-  return <div>UseRef3</div>;
+  const [showToDo, setShowToDo] = useState(false);
+
+  return (
+    <div>
+      {showToDo && <ToDo />}
+      <button
+        className='btn btn-primary'
+        onClick={() => {
+          setShowToDo(!showToDo);
+        }}
+      >
+        Toggle ToDo
+      </button>
+    </div>
+  );
 }
 
 export default UseRefExample3;
