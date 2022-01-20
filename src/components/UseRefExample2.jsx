526cf13747f86de30da602badce4ec8c19d6a46e
ahmed-razic <ahmed.razic@gmail.com>
2022-01-20 14:59:51 +0100

    added 3 comp

--- src/components/UseRefExample2.jsx
+++ src/components/UseRefExample2.jsx
@@ -1,5 +1,27 @@
-function UseRefExample2() {
-  return <div></div>;
-}
+import { useState, useEffect, useRef } from 'react';
 
+function UseRefExample2() {
+  const [name, setName] = useState('');
+  const renders = useRef(1);
+  const prevName = useRef('');
+  useEffect(() => {
+    renders.current += 1;
+    prevName.current = name;
+  }, [name]);
+
+  return (
+    <div>
+      <h1>Renders: {renders.current}</h1>
+      <h2>Name: {prevName.current}</h2>
+      <input
+        type='text'
+        value={name}
+        className='form-control mb-3'
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+    </div>
+  );
+}
 export default UseRefExample2;
---
+++ src/components/UseRefExample3.jsx
@@ -0,0 +1,5 @@
+function UseRefExample3() {
+  return <div>UseRef3</div>;
+}
+
+export default UseRefExample3;
