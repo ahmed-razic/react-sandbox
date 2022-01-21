534d7ab1c16cb66792a4d9fd6a9e94f52e4f3685
ahmed-razic <ahmed.razic@gmail.com>
2022-01-21 17:55:28 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import UseMemoExample from './components/UseMemoExample';
+import UseMemCallbackExample from './components/UseCallbackExample';
 
 function App() {
   return (
     <div className='container mt-5'>
-      <UseMemoExample />
+      <UseMemCallbackExample />
     </div>
   );
 }
